1ab14ef4087d05a654904acbd2ed7ee0bcfece98
ParanormalRave <147731636+ParanormalRave@users.noreply.github.com>
2025-12-02 07:29:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const togglemode = document.querySelector('.togglemode');
+const body = document.body;
+const links = document.querySelectorAll('.link');
+const iconcontaner = document.querySelector('.iconcontainer');
+const sun = document.querySelector('#sun');
+const moon = document.querySelector('#moon');
+const logoname = document.querySelector('.logoname');
+const view = document.querySelector('.view');
+
+
+togglemode.addEventListener('click', ()=>{
+    body.classList.toggle('darkmode');
+    links.forEach(link =>{
+        link.classList.toggle('lighttext');
+    })
+    iconcontaner.classList.toggle('sun');
+    moon.classList.toggle('display');
+    togglemode.classList.toggle('togglemodedark')
+    sun.style.display === 'none'? sun.style.display = 'block': sun.style.display = 'none';
+    sun.style.display === 'hidden'? sun.style.visibility = 'visible': sun.style.visibility = 'hidden';
+})
+
+logoname.addEventListener('click', ()=>{
+    location.reload();
+})
