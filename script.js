0fd24780035cbc53407456a08fbe0b4a4d3c6e1e
ParanormalRave <147731636+ParanormalRave@users.noreply.github.com>
2026-01-06 00:41:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const navsmall = document.querySelector('.nav-small');
 const exit = document.querySelector('.fa-xmark');
 const bars = document.querySelector('.fa-bars');
 const image = document.querySelector(".image");
+const circlehtml = document.querySelector('.html');
+const statscontainer = document.querySelector('.statscontainer');
+
 
 window.addEventListener('DOMContentLoaded', ()=>{
     image.classList.add("slidin");
@@ -51,3 +54,73 @@ window.addEventListener("scroll", () => {
 logoname.addEventListener('click', ()=>{
     location.reload();
 })
+const numberhtml = document.querySelector('#numberhtml span');
+let counter = 0;
+
+statscontainer.addEventListener('click', ()=>{
+    setInterval(()=>{
+        if(counter == 85){
+            clearInterval
+            numberhtml.innerHTML = 85;
+        }else{
+            counter++;
+            numberhtml.innerHTML = counter;   
+        }
+    }, 25)
+
+    circlehtml.classList.toggle("skillanimate");
+})
+
+let counterC = 0;
+const statscontainerc = document.querySelector('.statscontainerc');
+const circlecss = document.querySelector('.css');
+statscontainerc.addEventListener('click', ()=>{
+    setInterval(()=>{
+        if(counterC == 90){
+            clearInterval
+        }else{
+            counterC++;
+            document.querySelector("#numbercss").innerHTML = counterC + "%";   
+        }
+    }, 27)
+
+    circlecss.classList.toggle("canimate");
+})
+
+let counterB = 0;
+const statscontainerB = document.querySelector('.statscontainerB');
+const circleboot = document.querySelector('.boot');
+statscontainerB.addEventListener('click', ()=>{
+    setInterval(()=>{
+        if(counterB == 70){
+            clearInterval
+        }else{
+            counterB++;
+            document.querySelector("#numberboot").innerHTML = counterB + "%";   
+        }
+    }, 20)
+
+    circleboot.classList.toggle("banimate");
+})
+
+
+let counterJ = 0;
+const statscontainerJ = document.querySelector('.statscontainerJ');
+const circlejs = document.querySelector('.js');
+statscontainerJ.addEventListener('click', ()=>{
+    setInterval(()=>{
+        if(counterJ == 75){
+            clearInterval
+        }else{
+            counterJ++;
+            document.querySelector("#numberjs").innerHTML = counterJ + "%";   
+        }
+    }, 20)
+
+    circlejs.classList.toggle("janimate");
+})
+
+
+
+
+
